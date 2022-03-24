6b9a50c512ad2bdaa351f323da08362441c409c6
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-25 00:24:32 +0700

    build only client

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fib Calculator Fob Test Test Test Test Test Test
+          Fib Calculator Fob Test Test Test Test Test Test Test
         </a>
         {/* <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page NA</Link> */}
